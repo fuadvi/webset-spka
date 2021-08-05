963abce0e72056a33b3ee75f7cf2756d0b462b02
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-08-05 14:39:01 +0700

    buat migration kontrol dan buat model kontrol

---
+++ app/Models/Kontrol.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kontrol extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'tgl', 'jam', 'server', 'deskripsi_masalah', 'kategori',
+        'deskripsi_penyelesaian', 'koordinasi', 'ket'
+    ];
+
+    // protected $casts = [
+    //     'created_at' => 'datetime:Y-m-d H:m:s',
+    //     'updated_at' => 'datetime:Y-m-d H:m:s',
+    // ];
+}
