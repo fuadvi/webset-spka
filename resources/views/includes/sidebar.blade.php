feabc132864c252a9d6b66ce396db7b501a05458
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2021-08-07 15:24:16 +0700

    membuat aktif directory

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -14,33 +14,33 @@
       <hr class="sidebar-divider my-0">
 
       <!-- Nav Item - SKPA -->
-      <li class="nav-item active">
+      <li class="nav-item  {{ request()->is('/') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('/') }}">
           <i class="fas fa-fw fa-home"></i>
           <span>DASHBOARD</span></a>
       </li>
 
       <!-- Nav Item - SKPA -->
-      <li class="nav-item active">
+      <li class="nav-item {{ request()->is('spka') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('spka.index') }}">
           <i class="fas fa-fw fa-building"></i>
           <span>Data SKPA</span></a>
       </li>
       <!-- Nav Item - Email -->
-      <li class="nav-item active">
+      <li class="nav-item {{ request()->is('data-email') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('data-email.index') }}">
           <i class="fas fa-fw fa-inbox"></i>
           <span>Data Email</span></a>
       </li>
       <!-- Nav Item - Email -->
-      <li class="nav-item active">
+      <li class="nav-item {{ request()->is('kontrol') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('kontrol.index') }}">
           <i class="fas fa-fw fa-bug"></i>
           <span>Data Kontrol Masalah</span></a>
       </li>
 
       <!-- Nav Item - user -->
-      <li class="nav-item active">
+      <li class="nav-item {{ request()->is('user') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('/user') }}">
           <i class="fas fa-fw fa-users"></i>
           <span>Data User</span></a>
@@ -48,7 +48,7 @@
 
       @if (Auth::user()->level == 'user')
       <!-- Nav Item - ganti password -->
-      <li class="nav-item active">
+      <li class="nav-item {{ request()->is('user/change/'.Auth::user()->id) ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('user.newPassword', Auth::user()->id) }}">
           <i class="fas fa-fw fa-sliders-h"></i>
           <span>Ganti Password</span></a>
